da00052f82ec58352a415b0c96aa8bb06e04c39f
yuminghao <yuminghao@kuaishou.com>
2023-09-10 16:41:08 +0800

    chore: finish

--- src/autojump.ts
+++ src/autojump.ts
@@ -1,16 +1,19 @@
-import { Item } from "./workflow";
+import { Item } from './workflow';
 
 // FIXME: 想办法把 exec 封装到这里
 export function getAutoJumpPath(query: string[]): Item[] {
-    return query.map((item) => {
+    const pathList = query.map((item) => item.replace(/^.+?__[1-9]__/, ''));
+    // 去重
+    const set = new Set<string>(pathList);
+
+    return Array.from(set).map((item) => {
         const last = item.match(/\/([^/]+)$/);
         const title = last ? last[1] : item;
-        const subtitle = item.replace(/^.+?__[1-9]__/, '');
 
         return {
             title,
-            subtitle,
-            arg: subtitle,
+            subtitle: item,
+            arg: item,
             valid: true,
         };
     });
